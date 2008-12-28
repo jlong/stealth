d155439bdada7784fad16b5a82925e2df91a5762
John W. Long <me@johnwlong.com>
2008-12-27 19:56:11 -0500

    added ship

--- javascripts/game.js
+++ javascripts/game.js
@@ -4,13 +4,47 @@ function image(src) {
   return img;
 }
 
+var Sprite = Class.create({
+  setPosition: function(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+});
+
+var Star = Class.create(Sprite, {
+  initialize: function(x, y, velocity) {
+    this.setPosition(x, y)
+    this.setVelocity(velocity);
+  },
+  
+  draw: function(context) {
+    context.drawImage(Game.starImage, this.frameStartX, 0, 5, 5, this.x, this.y, 5, 5);
+  },
+  
+  setVelocity: function(velocity) {
+    this.velocity = velocity;
+    this.frameStartX = (this.velocity - 1) * 5;
+  }
+});
+
+var Ship = Class.create(Sprite, {
+  initialize: function(x, y) {
+    this.setPosition(x, y);
+  },
+  
+  draw: function(context) {
+    context.drawImage(Game.shipImage, this.x - 50, this.y - 38);
+  }
+});
+
 var Game = Class.create({
   initialize: function(canvas) {
-    this.setCanvas(canvas);
-    this.stars = [];
     this.paused = true;
-    this.timeout = 1000 / Game.frameRate
-    for (var i = 0; i < 100; i++) this.stars.push(new Object());
+    this.timeout = 1000 / Game.frameRate;
+    this.setCanvas(canvas);
+    this.ship = new Ship(50, 50);
+    this.stars = [];
+    for (var i = 0; i < 100; i++) this.stars.push(new Star());
   },
   
   animateStars: function() {
@@ -25,18 +59,20 @@ var Game = Class.create({
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
   
+  drawShip: function() {
+    this.ship.draw(this.context);
+  },
+  
+  drawStars: function() {
+    for (var i = 0; i < this.stars.size(); i++) this.stars[i].draw(this.context);
+  },
+  
   main: function() {
     this.clearCanvas();
     this.animateStars();
     this.drawStars();
+    this.drawShip();
     this.setTimeout();
-  },
-  
-  drawStars: function() {
-    for (var i = 0; i < this.stars.size(); i++) {
-      var star = this.stars[i];
-      this.context.drawImage(Game.starImage, star.frameStartX, 0, 5, 5, star.x, star.y, 5, 5);
-    }
   },
   
   pause: function() {
@@ -48,10 +84,8 @@ var Game = Class.create({
         height = this.canvas.height - 5;
     for (var i = 0; i < this.stars.size(); i++) {
       var star = this.stars[i];
-      star.x = Math.floor(Math.random() * width);
-      star.y = Math.floor(Math.random() * height);
-      star.velocity = Math.floor(Math.random() * 10) + 1;
-      star.frameStartX = (star.velocity - 1) * 5
+      star.setPosition(Math.floor(Math.random() * width), Math.floor(Math.random() * height));
+      star.setVelocity(Math.floor(Math.random() * 10) + 1);
     }
   },
   
@@ -59,8 +93,10 @@ var Game = Class.create({
     this.canvas = canvas = $(canvas);
     this.canvas.setStyle({
       background: 'black',
+      cursor: 'none',
       padding: 0
-    })
+    });
+    this.canvas.observe('mousemove', this.onmousemove.bind(this));
     this.context = canvas.getContext('2d');
   },
   
@@ -71,6 +107,10 @@ var Game = Class.create({
   start: function() {
     this.paused = false;
     this.setTimeout();
+  },
+  
+  onmousemove: function(event) {
+    this.ship.setPosition(Event.pointerX(event), Event.pointerY(event));
   }
 });
 
